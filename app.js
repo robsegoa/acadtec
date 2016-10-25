338fb028aaf887bf6bd699bd59c4acb46bff9600
Robson Segoa <robson.sukita@gmail.com>
2016-10-25 08:12:05 -0200

    antes de excluir os routes

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var load = require('express-load')
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -23,14 +23,21 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({ secret: 'nodejsacadtec009933'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//Essa 4 linhas abaixo foram comentadas pq as rotas foram criadas nos controladores para usar o load;
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
+//app.use('/', routes); 
+//app.use('/users', users);
 
 //middleware
 app.use(erros.notfound);
 app.use(erros.serverError);
+
+load('controllers').then('models').then('routes').into(app);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
