cafd32bcdd17ab114b8e290a24652a7741c2efd4
Robson Michkinis Segoa <robson.segoa@neoway.com.br>
2016-12-28 11:28:59 -0200

    Ajuste de acesso ao banco

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var expressValidator = require('express-validator');
 
 
 //conexão com mongo
-mongoose.connect('mongodb://robson.segoa:!@ds145138.mlab.com:45138/acadtec',function(err){
+mongoose.connect('mongodb://acadtec:123456@ds145138.mlab.com:45138/acadtec',function(err){
   if(err){
     console.log("Erro ao conectar no mongoDB: "+err);
   }else{
